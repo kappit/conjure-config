bbaf50668203f38aafe2bb0c0838a8644579a2b2
Kappit <kasper.h90@outlook.dk>
2021-01-16 18:07:36 +0100

    Fixed error: 'must not be accessed before initialization'.

--- src/Config.php
+++ src/Config.php
@@ -4,20 +4,33 @@ declare(strict_types=1);
 
 namespace Conjure\Config;
 
+/**
+ * Class Config
+ * @package Conjure\Config
+ */
 final class Config
 {
 
-    private static self|null $instance;
+    /**
+     * @var self|null
+     */
+    private static self|null $instance = null;
 
+    /**
+     * Config constructor.
+     */
     private function __construct()
     {
         // To do
         echo 'Config singleton has been constructed';
     }
 
+    /**
+     * @return static
+     */
     public static function getInstance(): self
     {
-        if (!self::$instance) {
+        if (self::$instance === null) {
             self::$instance = new self();
         }
 
