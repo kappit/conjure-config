93e3e39712a6fe7648dff4f942e9177f5cddb2e6
Kappit <kasper.h90@outlook.dk>
2021-01-23 10:44:25 +0100

    Added implementation for Config __constructor.,

--- src/Config.php
+++ src/Config.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Conjure\Config;
 
+use Composer\Autoload\ClassLoader;
+use ReflectionClass;
+use ReflectionException;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Dotenv\Exception\FormatException;
+use Symfony\Component\Dotenv\Exception\PathException;
+
 /**
  * Class Config
  * @package Conjure\Config
@@ -14,15 +21,48 @@ final class Config
     /**
      * @var self|null
      */
-    private static self|null $instance = null;
+    private static self|null $_instance = null;
+
+    /**
+     * @var Dotenv
+     */
+    private Dotenv $_dotenv;
+
+    /**
+     * @var string
+     */
+    private string $_root;
 
     /**
      * Config constructor.
      */
     private function __construct()
     {
-        // To do
-        echo 'Config singleton has been constructed';
+        try {
+            $rc = new ReflectionClass(objectOrClass: ClassLoader::class);
+            $this->_root = dirname(path: $rc->getFileName(), levels: 2);
+            $this->_dotenv = new Dotenv();
+            $this->_dotenv->loadEnv(path: $this->_root . '/.env');
+        } catch (ReflectionException | FormatException | PathException $ex) {
+            $className = get_class(object: $ex);
+
+            $error = match ($className) {
+                ReflectionException::class => 'Composer is not installed',
+                FormatException::class => 'The .env file has a syntax error',
+                PathException::class => 'The file .env does not exist in '
+                    . $this->_root
+            };
+
+            die($error);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getRoot(): string
+    {
+        return $this->_root;
     }
 
     /**
@@ -30,11 +70,11 @@ final class Config
      */
     public static function getInstance(): self
     {
-        if (self::$instance === null) {
-            self::$instance = new self();
+        if (self::$_instance === null) {
+            self::$_instance = new self();
         }
 
-        return self::$instance;
+        return self::$_instance;
     }
 
 }
